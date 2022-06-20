37bb148a00a306fd1174e21f71ed69e40162f5ce
Dmitriy Kostyanetskiy (Arcadia) <dmitriy.kostyanetskiy@arcadia.spb.ru>
2022-06-20 22:19:38 +0200

    chore: update linter

--- functions/src/bootstrap.ts
+++ functions/src/bootstrap.ts
@@ -1,20 +1,22 @@
-import * as admin from 'firebase-admin'
+import * as admin from 'firebase-admin';
 
-import { CloudFunction, HttpsFunction } from 'firebase-functions'
-import Container, { ObjectType } from 'typedi'
+import { CloudFunction, HttpsFunction } from 'firebase-functions';
+import { Container, Constructable } from 'typedi';
 
-import { FunctionCreator } from './functions/function-creator'
-import { PubSub } from '@google-cloud/pubsub'
+import { FunctionCreator } from './functions/function-creator';
+import { PubSub } from '@google-cloud/pubsub';
 
-export type CreateFunction = (type: ObjectType<FunctionCreator>) => CloudFunction<unknown> | HttpsFunction;
+export type CreateFunction = (
+  type: Constructable<FunctionCreator>
+) => CloudFunction<unknown> | HttpsFunction;
 
 export default function bootstrap(): CreateFunction {
   admin.initializeApp({
-    credential: admin.credential.applicationDefault()
-  })
+    credential: admin.credential.applicationDefault(),
+  });
 
-  Container.set(admin.firestore.Firestore, admin.firestore())
-  Container.set(PubSub, new PubSub())
+  Container.set(admin.firestore.Firestore, admin.firestore());
+  Container.set(PubSub, new PubSub());
 
-  return (type) => Container.get<FunctionCreator>(type).createFunction()
+  return (type) => Container.get<FunctionCreator>(type).createFunction();
 }
